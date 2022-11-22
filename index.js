265ab7ecd2e60ac955262fd54adb2763af3962f1
ryan605 <langatryan26@gmail.com>
2022-11-22 21:10:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector("#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Brazil is the champion";
